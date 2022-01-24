0f336907e1ac499067c4d27be18dca5c9aa2fd3a
Bexruz <bexruzmurodov71@gmail.com>
2022-01-24 13:33:04 +0500

    1-dars

--- main.py
+++ main.py
@@ -1,1 +1,88 @@
 print("Assalomualeykum")
+# print(1)
+# print(1,2)
+# print(1+4)
+# print("bir" + "ikki")
+# print('bir', 'ikki')
+# print("PC")
+# print("""Ko'p qataorli
+# matn""")
+# print("\" - qo'shtirnoq ")
+# print('\" qo\'shtirnoq ')
+# print("\' - birtirnoq")
+# print('Birinchi qator\nIkkinchi qator')
+# print('Birinchi ustun\t\tIkkinchi ustun')
+# print("Assalomu aleykum")
+# print('Assalomu aleykum')
+# print('Mening ismim "Bexruz"')
+# print("Mening ismim 'Bexruz'")
+# print('Mening ismim \'Bexruz\'' )
+# print("Mening ismim \"Bexruz\"")
+# print("""Bu mening birinchi darsim
+# Birinchi martta printni o'rgandik""")
+# print("Ismim:","Bexruz", "Yoshim:", "19")
+# print("To'rtburchak eni 4, bo'yi 5, \nTo'rtburchak yuzi :", 4*5)
+# print("Ismim:\tBexruz\nYoshim:\t19")
+# print("Men Dasturchiman")
+# print("Men 'mohir' dasturchiman")
+# print("""Birinchi kuni juda ajoyib o'tdi.
+# Chunki zo'r "motivatsiya" bilan borgandim.
+# Keyin esa motivatsiyam yoqolib qoldi.
+# Keyin tushundimki inson motivatiya emas, ma'sulyatni his qilish, javobgarlikni olish, muntazamlik, tartib - intizom muhim ekan.""")
+# print(10/6)
+# print(10/5)
+# print(10-5)
+# print(10+5)
+# print(10//3)
+# print(10%3)
+# print(3 ** 4)
+# print((2 ** 3 + 10/5) // 5)
+# print(10 + 20)
+# print(365//7)
+# print("200 kunda", 200//7, "hafta bor")
+# print("Assalomu aleykum " + input("Ism kiriting: ").title())
+# print("So'z uzunligi: ", len(input("So'z kiriting: ")),  "ta belgidan iborat")
+# print("35 ni 8 ga bo'laganda", 35%8, "qoldoq qolad")
+# print("35 ni 8 ga bo'lib butun qimini olsa", 34//8,"bo'ladi")
+# print(" '+' belgisi ikki sonni qo'shish uchun ishlatiladi.")
+# print("""Birinchi dars
+#
+# "+" belgisi ikki sonni qo'shish uchun ishlatiladi.
+# print funksiyasi: print("Assalomu " + "alaykum")
+# Shuningdek '+' belgisi ikki satrni qo'shish uchun ham ishlatiladi
+# """)
+# print(input("Daftarlar sonini kiriting: ") + " daftar borekan")
+# print(input("Ismingizni kiriting: ").title() + ", siz tizimga muvaffaqiyatli kirdingiz")
+# print( "Agar kiritilgan belgini 3 marta ko'paytirsak", input("Belgini kiriting: ")*3)
+# print("""
+# ***********
+# *********
+# *******
+# *****
+# ***
+# *
+# """)
+# print("""
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+# ***********
+# """)
+# print("""
+#  ______________
+# |              |
+# |              |
+# |              |
+# |              |
+# |______________|
+# """)
+# print("""
+#      /\
+#     /  \
+#    /    \
+#   /      \
+#  /        \
+# /__________\
+# """)
